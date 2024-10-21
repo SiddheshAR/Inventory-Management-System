d453dad777b6eb3028247a82a86ba5c252493fb1
SiddheshAR <siddheshfr@gmail.com>
2024-10-22 02:20:45 +0530

    Layout & Redux Config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import DashboardWrapper from "./dashboardWrapper";
+import DashboardLayout from "./dashboardWrapper";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,10 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <DashboardLayout>
         {children}
+        </DashboardLayout>
       </body>
     </html>
   );
